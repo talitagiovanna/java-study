3f22df1701349be4278e56cbf313a1a2782678ee
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2023-03-06 12:30:09 -0300

    aprendendo a mostrar hora, data e dia da semana

---
+++ Main.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Date;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Date relogio = new Date();
+		System.out.println("A hora do sistema é");
+		System.out.println(relogio.toString());
+	}
+}

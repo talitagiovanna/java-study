f5c40d6ce028fa0842b296552c190e38fd2b2e12
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2023-04-25 14:15:55 -0300

    Entrada de dados em Java - Parte 2

---
+++ Aula2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/*public class Aula2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s1, s2, s3;
+
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("Dados Digitados: ");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        sc.close();
+    }
+}*/
+
+public class Aula2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x;
+        String s1, s2, s3;
+
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("Dados Digitados: ");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        sc.close();
+    }
+}

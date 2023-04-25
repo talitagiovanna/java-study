da90aebe0f8e4960587f7d1af6913360cc9b8466
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2023-04-25 10:53:37 -0300

    Processamento de dados em Java, Casting.

---
+++ Exercicio6.java
@@ -0,0 +1,30 @@
+/*public class Exercicio6 {
+
+    public static void main(String[] args) {
+
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b;
+
+        System.out.println(resultado);
+    }
+}*/
+
+public class Exercicio6 {
+
+    public static void main(String[] args) {
+        
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+
+    }
+}

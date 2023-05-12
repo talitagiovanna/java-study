82497a056c12def7d881e11c0335d002ef51e9aa
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2023-05-12 13:06:44 -0300

    Expressão condicional ternária

---
+++ Aula6.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/*public class Aula6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double preco = 34.5;
+        double desconto;
+        if(preco < 20.0) {
+            desconto = preco * 0.1;
+        }
+        else {
+            desconto = preco * 0.05;
+        }
+        System.out.println(desconto);
+        sc.close();
+    }
+}
+*/
+
+public class Aula6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
+        
+        sc.close();
+    }
+}

657f134b4c12aab0d6979d089f61339ac6aab68a
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2023-05-22 15:07:11 -0300

    Funções interessantes para String

---
+++ Funcao_String2.java
@@ -0,0 +1,15 @@
+public class Funcao_String2 {
+
+    public static void main(String[] args) {
+
+        String s = "potato apple lemon orange";
+
+        String[] vect = s.split(" ");
+
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+        System.out.println(vect[3]);
+
+    }
+}

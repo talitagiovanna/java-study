d25b54a0c7840d01d123aa60f6759c76007cc007
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2023-05-23 11:01:26 -0300

    Funções (sintaxe)

---
+++ Funcoes_basicas.java
@@ -0,0 +1,10 @@
+public class Funcoes_basicas {
+
+    public static void main(String[] args) {
+        
+        double y = 25.0;
+        double x = Math.sqrt(y);
+
+        System.out.println(x);
+    }
+}
